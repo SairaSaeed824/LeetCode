59131f337bb8af7396752d32d1de79f543f224be
SairaSaeed824 <161067919+SairaSaeed824@users.noreply.github.com>
2024-08-07 11:34:47 +0500

    Time: 11 ms (90.67%), Space: 21.9 MB (40.94%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int  nonZero=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                    nums[nonZero]=nums[i];
+                nonZero++;
+                    
+                }
+            }
+               for(int i=nonZero;i<nums.size();i++){
+                   nums[i]=0;
+               }
+        
+    }
+};
