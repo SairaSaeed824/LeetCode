078ed555b1a2150565db0d068c4d8eab29dab19c
SairaSaeed824 <161067919+SairaSaeed824@users.noreply.github.com>
2024-08-10 13:31:36 +0500

    Time: 1716 ms (5.01%), Space: 715 MB (5.01%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        for(int i=s.size()-1;i>=0;i--){
+            if(i==0){
+                return false;
+            }
+            string sub=s.substr(0,i);
+            if(s+sub==sub+s){
+                return true;
+            }
+        }
+        return false;
+    }
+};
